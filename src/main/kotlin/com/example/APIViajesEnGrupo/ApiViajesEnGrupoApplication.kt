09aa26fdafb4fe8e9c3011c5f01f8e7109e21b8a
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2024-12-12 20:00:32 +0100

    Proyecto SpringBoot
    Creado el proyecto SpringBoot con las dependencias necesarias

---
+++ src/main/kotlin/com/example/APIViajesEnGrupo/ApiViajesEnGrupoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.APIViajesEnGrupo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiViajesEnGrupoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiViajesEnGrupoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/APIViajesEnGrupo/ApiViajesEnGrupoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.APIViajesEnGrupo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiViajesEnGrupoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
